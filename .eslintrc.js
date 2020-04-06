65450b62113c542c3a380a1727514c6c9dbc3098
andreitol <tolstoi.a@gmail.com>
2020-04-06 11:51:32 +0300

    lesson-1

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["plugin:@typescript-eslint/recommended", "prettier/@typescript-eslint",'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-max-props-per-line': [1, { maximum: 2, when: 'multiline' }],
+  },
+};
