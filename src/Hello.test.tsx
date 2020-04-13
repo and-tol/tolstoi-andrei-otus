f6a3ba6a1192a7ad664714fb2dc908aff3527f22
andreitol <tolstoi.a@gmail.com>
2020-04-13 18:37:40 +0300

    feat: add jestSettings(enzyme). fix: Hello.test

--- src/Hello.test.tsx
+++ src/Hello.test.tsx
@@ -5,6 +5,8 @@ import { Hello } from './Hello';
 
 describe('Write name check', () => {
   it('should render a document title', () => {
-    expect(shallow(<Hello />).text).toEqual('Hello World, Vasya!');
+    expect(shallow(<Hello username="Ivan" />).text()).toEqual(
+      'Hello World, Ivan!',
+    );
   });
 });
